90f78d3ba0302e5eb7fde9eeda07d16c5bb1c3e2
pankaj.makwana <bitwpankaj@gmail.com>
2022-06-08 14:31:05 +0530

    Testing PR

--- app/src/main/java/com/example/bitrisetestproject/MainActivity.kt
+++ app/src/main/java/com/example/bitrisetestproject/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // I have added this comment to just test pull request attempt 2
+        // I have added this comment to just test pull request attempt 3
     }
 }
